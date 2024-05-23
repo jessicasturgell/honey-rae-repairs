7121ff8eaaece55dbd06ccb017ce6a34ec77d258
Jessica Sturgell <sturgell01@gmail.com>
2024-05-23 14:56:22 -0400

    added navbar and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,25 @@ import "./App.css"
 import { TicketList } from "./components/tickets/TicketList.jsx"
 import { CustomerList } from "./components/customers/CustomersList.jsx"
 import { EmployeeList } from "./components/employees/Employees.jsx"
+import { NavBar } from "./components/nav/NavBar.jsx"
+import { Routes, Route, Outlet } from "react-router-dom"
 
 export const App = () => {
-  return <>
-  <TicketList />
-  <CustomerList />
-  <EmployeeList />
-  </>
+  return (
+  <Routes>
+    <Route 
+      path="/" 
+      element={
+        <>
+          <NavBar />
+          <Outlet />
+        </>
+      }
+    >
+      <Route path="/tickets" element={<TicketList />} />
+      <Route path="/customers" element={<CustomerList />} />
+      <Route path="/employees" element={<EmployeeList />} />
+    </Route>
+  </Routes>
+  )
 }
